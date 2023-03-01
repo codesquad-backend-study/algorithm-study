c3621d931976ff7cb123c6214abe58b05f48cb28
Sully <2dcoder@naver.com>
2023-03-01 23:12:11 +0900

    Sully : Programmers / 크레인 인형뽑기 / Java / 2차 / 성공

--- programmers/sully/week5/크레인_인형뽑기_Sully.java
+++ programmers/sully/week5/크레인_인형뽑기_Sully.java
@@ -1,33 +1,31 @@
 package programmers.sully.week5;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class 크레인_인형뽑기_Sully {
     public static int solution(int[][] board, int[] moves) {
         int answer = 0;
-        int start = 0;
-        List<Integer> basket = new ArrayList<>();
+        Stack<Integer> basket = new Stack<>();
+        basket.push(0);
 
         // 가장 아래 칸부터 쌓이고, 크레인은 가장 위 칸부터 뽑으니까
-        for (int i = board.length - 1; i > 0; i--) {
+        for (int move : moves) {
             for (int j = 0; j < board.length; j++) {
-                for (int k = start; k < moves.length; k++) {
-                    if (moves[k] - 1 == board[i][j]) {
-                        basket.add(moves[k] - 1);
-                        start++;
-                        break;
-                    }
-                }
-            }
-        }
+                int tmp = board[j][move - 1];
 
-        // 연속된 배열 삭제할 떄마다 카운트
-        int current = basket.get(0);
-        for (Integer b : basket) {
-            if (b != current) {
-                current = b;
-                answer++;
+                if (tmp == 0) {
+                    continue;
+                }
+
+                if (basket.peek() == tmp) {
+                    basket.pop();
+                    answer += 2;
+                } else {
+                    basket.push(tmp);
+                }
+
+                board[j][move - 1] = 0;
+                break;
             }
         }
 
