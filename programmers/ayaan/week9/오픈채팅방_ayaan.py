7d9b07bbdd597103c4d1253fcb98d133d668f1db
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-04-01 20:36:48 +0900

    오픈채팅방  Python 2차

--- programmers/ayaan/week9/오픈채팅방_ayaan.py
+++ programmers/ayaan/week9/오픈채팅방_ayaan.py
@@ -1,22 +1,25 @@
 def solution(record):
-    position = {}
+    nickName = {}
+    messages = []
     result = []
     
     for i, s in enumerate(record):
         data = s.split()
         
-        if data[0] == "Enter":
-            position[data[1]] = [i]
-            result.append(data[2] + "님이 들어왔습니다.")
-        elif data[0] == "Leave":
-            position[data[1]].append(i)
-            result.append(data[2] + "님이 나갔습니다.")
+        if data[0] != "Leave":
+            nickName[data[1]] = data[2]
+        messages.append(data[0] + " " + data[1])
+        
+    for message in messages:
+        command, uid = message.split()
+        
+        if command == "Enter":
+            result.append(nickName[uid] + "님이 들어왔습니다.")
+        elif command == "Leave":
+            result.append(nickName[uid] + "님이 나갔습니다.")
         else:
-            history = position[data[1]]
-            for idx in history:
-                a = result[idx]
-                nim = a.find("님")
-                result[idx] = data[2] + a[nim:]
-    print(result)
+            continue
+    
+    return result
     
 solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
