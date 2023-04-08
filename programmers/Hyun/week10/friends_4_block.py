c2ecc761ff9e84525005d3b40910b338b8e56add
Hyun <ghkdgus29@naver.com>
2023-04-08 12:08:15 +0900

    해결

--- programmers/Hyun/week10/friends_4_block.py
+++ programmers/Hyun/week10/friends_4_block.py
@@ -1,59 +1,45 @@
-board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
-# board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
-m = 6  # height
-n = 6  # width
-
-
-# answer = 15
-
-def mask(sx, sy, columns, check):
+def mask(sx, sy, columns, check, deleted):
     if columns[sx][sy] == '0':
         return
 
     if columns[sx][sy] == columns[sx + 1][sy] == columns[sx][sy + 1] == columns[sx + 1][sy + 1]:
         check[sx][sy] = check[sx + 1][sy] = check[sx][sy + 1] = check[sx + 1][sy + 1] = True
-        global delete_flag
-        delete_flag = True
+
+        deleted[0] = True
 
 
-columns = [[] for _ in range(n)]
-check = [[False] * m for _ in range(n)]
-
-for blocks in board:
-    for idx, block in enumerate(blocks):
-        columns[idx].append(block)
-
-delete_flag = False
-ans = 0
-
-for y in range(m - 1):
-    for x in range(n - 1):
-        mask(x, y, columns, check)
-
-print(columns)
-print(check)
-
-while delete_flag:
-    delete_flag = False
-
-    for y in range(m):
-        for x in range(n):
-            if check[x][y]:
-                ans += 1
-
-    for x, column in enumerate(columns):
-        erase_cnt = sum(check[x])
-        column = [block for y, block in enumerate(column) if not check[x][y]]
-        columns[x] = ['0'] * erase_cnt + column
-
-    print(columns)
-    print(check)
-
+def solution(m, n, board):
+    columns = [[] for _ in range(n)]
     check = [[False] * m for _ in range(n)]
+
+    for blocks in board:
+        for idx, block in enumerate(blocks):
+            columns[idx].append(block)
+
+    deleted = [False]
+    ans = 0
 
     for y in range(m - 1):
         for x in range(n - 1):
-            mask(x, y, columns, check)
+            mask(x, y, columns, check, deleted)
 
-# print(columns)
-print(ans)
+    while deleted[0]:
+        deleted[0] = False
+
+        for y in range(m):
+            for x in range(n):
+                if check[x][y]:
+                    ans += 1
+
+        for x, column in enumerate(columns):
+            erase_cnt = sum(check[x])
+            column = [block for y, block in enumerate(column) if not check[x][y]]
+            columns[x] = ['0'] * erase_cnt + column
+
+        check = [[False] * m for _ in range(n)]
+
+        for y in range(m - 1):
+            for x in range(n - 1):
+                mask(x, y, columns, check, deleted)
+
+    return ans
