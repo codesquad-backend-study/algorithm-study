6aaab7ace7a771a553596a1d874529f8061e95dc
Hyun <ghkdgus29@naver.com>
2023-04-08 17:48:02 +0900

    ㅋㅋㅋ 해결

--- programmers/Hyun/week10/menu_renewal.py
+++ programmers/Hyun/week10/menu_renewal.py
@@ -1,49 +1,39 @@
-orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
-course = [2, 3, 4]
-# 메뉴 개수
+import itertools
+import time
 
-result = ["AC", "ACDE", "BCFG", "CDE"]
-# 코스 메뉴 구성
-
-menu_counter = {}
-for order in orders:
-    for menu in order:
-        if menu not in menu_counter:
-            menu_counter[menu] = 1
-        else:
-            menu_counter[menu] += 1
-
-menu_counter = sorted(menu_counter.items(), key=lambda x: x[1], reverse=True)
-print(menu_counter)
-
-menu_guest = {}
-
-for idx, order in enumerate(orders):
-    for menu in order:
-        if menu not in menu_guest:
-            menu_guest[menu] = []
-
-        menu_guest[menu].append(idx)
-
-print(menu_guest)
-
-ans = []
-for menu, menu_count in menu_counter:
-    tmp = []
-    for cut in course:
-        if cut <= menu_count:
-
-            maximum = 0
-            fivot_guests = menu_guest[menu]
-            for other_menu, other_menu_count in menu_counter:
-                if menu == other_menu:
-                    continue
-
-                other_guests = menu_guest[other_menu]
-
-                fivot_guests.
+nums = [i for i in range(20)]
+answer_list = []
 
 
 
+def nCr(n, ans, r):
+    if len(ans) > r:
+        return
+
+    if n == len(nums):
+        if len(ans) == r:
+            temp = [i for i in ans]
+            answer_list.append(temp)
+        return
+    ans.append(nums[n])
+    nCr(n + 1, ans, r)
+    ans.pop()
+    nCr(n + 1, ans, r)
 
 
+start = time.time()
+
+nCr(0, [], 3)
+
+end = time.time()
+
+print(f"1 : {end-start:.5f}sec ")
+
+start = time.time()
+
+combinations = itertools.combinations([i for i in range(20)], 3)
+
+end = time.time()
+
+
+print(f"2 : {end-start:.5f}sec")
