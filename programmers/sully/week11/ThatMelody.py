77a4fa104409e8fdb87b40a08ac150ed3092b4ad
Sully <2dcoder@naver.com>
2023-04-16 14:16:39 +0900

    헤업..

--- programmers/sully/week11/ThatMelody.py
+++ programmers/sully/week11/ThatMelody.py
@@ -1,11 +1,11 @@
 def solution(m, musicinfos):
     answer = '(None)'
-    m = m.replace('C#', 'C').replace('D#', 'D').replace('F#', 'F').replace('G#', 'G').replace('A#', 'A')
+    m = m.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f').replace('G#', 'g').replace('A#', 'a')
 
     for mus in musicinfos:
         start, end, title, music = mus.split(',')
         # 위에서 했던 것처럼 리플레이스
-        music = music.replace('C#', 'C').replace('D#', 'D').replace('F#', 'F').replace('G#', 'G').replace('A#', 'A')
+        music = music.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f').replace('G#', 'g').replace('A#', 'a')
         # start, end 미닛으로 변환 + 음은 1분에 1개씩 재생됨 (즉, time이 재생되는 횟수이기도 함)
         time = (int(end.split(':')[0]) * 60 + int(end.split(':')[1])) - (
                 int(start.split(':')[0]) * 60 + int(start.split(':')[1]))
@@ -24,12 +24,24 @@ def solution(m, musicinfos):
             i += 1
 
         # tmp_music 전체가 m에 포함이 되면 (find, in 쓰면 안 됨)
+        # 더 긴 거?
+        title_time = {}
+        maxTime = 0
         for i in range(len(tmp_music)):
-            if tmp_music[i] == m[0]:
-                if tmp_music[i:len(m) + i] == m:
+            if tmp_music[i:len(m) + i] == m:
+                # 조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다.
+                # 재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
+                # 현재 시간이 더 길 경우
+                if maxTime < time:
                     answer = title
-                    break
+                    maxTime = time
+                    title_time[title] = time
+                # 재생된 시간이 더 작거나 같을 경우
+                else:
 
+
+                # title_time[title] = time
+                # answer = title
     return answer
 
 
