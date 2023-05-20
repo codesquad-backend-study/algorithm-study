e18b9b226185e7b943ba423d42fc99be7fcc92a0
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-05-20 15:50:36 +0900

    Ayaan : 561. Array Partition

--- leetcode/ayaan/week15/two_sum.py
+++ leetcode/ayaan/week15/two_sum.py
@@ -1,5 +1,12 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+def twoSum(nums, target):
+    nums_map = {}
+    for i, num in enumerate(nums):
+        nums_map[num] = i
+    print(nums_map)
+    for i, num in enumerate(nums):
+        if target - num in nums_map and i != nums_map[target - num]:
+            print([i, nums_map[target - num]])
+            return [i, nums_map[target - num]]
+
+
+twoSum([3,2,4], 6)
