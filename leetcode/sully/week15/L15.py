4be08264d3a3f34d2bcd5e44e4a9a4abeb8302f3
Sully <2dcoder@naver.com>
2023-05-20 15:26:33 +0900

    Sully: LeetCode / 15번 - 미디움 / Python / 리팩토링 (continue 추가)

--- leetcode/sully/week15/L15.py
+++ leetcode/sully/week15/L15.py
@@ -6,13 +6,15 @@ class Solution:
     # 완전 탐색으로 하려면 n^3 될 거 같은데 투포인터로 한번 n^2 만들어 보자
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         set_answer = set()
-        answer = []
 
         # 구현하기 편하도록 오른차순 정렬해 두자
         nums.sort()
 
         # 일단 이 반복문이 전체 뼈대를 잡아줄 수 있도록 하자
         for i, num in enumerate(nums):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
             # nums[i]를 중심으로 [i + 1 : -1]까지의 원소들을 투포인터로 각각 비교하는 방법 시도해 보자
             lt, rt = i + 1, len(nums) - 1
             # 이렇게 하면 n^3으로 풀 수밖에 없을 거 같은데 흠..
