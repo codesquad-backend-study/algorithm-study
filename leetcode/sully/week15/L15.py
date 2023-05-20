0385565b67d331443b73051ae68bdef3f808a8ed
Sully <2dcoder@naver.com>
2023-05-20 14:59:24 +0900

    Sully: LeetCode / 15번 - 미디움 / Python / 성공

--- leetcode/sully/week15/L15.py
+++ leetcode/sully/week15/L15.py
@@ -5,6 +5,7 @@ class Solution:
     # 3개 더해서 0이 되는 경우의 배열을 계속 append
     # 완전 탐색으로 하려면 n^3 될 거 같은데 투포인터로 한번 n^2 만들어 보자
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        set_answer = set()
         answer = []
 
         # 구현하기 편하도록 오른차순 정렬해 두자
@@ -16,9 +17,19 @@ class Solution:
             lt, rt = i + 1, len(nums) - 1
             # 이렇게 하면 n^3으로 풀 수밖에 없을 거 같은데 흠..
             while lt < rt:
-                # 이제 lt는 가만히 있고, rt가 또 한 번의 반복문 돌아주면서 lt 바로 전까지 rt -= 1을 해주면 되긴 할 듯
+                tmp_sum = num + nums[lt] + nums[rt]
 
-                if num + nums[lt] + nums[rt] == 0:
-                    answer.append([num, nums[lt], nums[rt]])
+                if tmp_sum < 0:
+                    lt += 1
+                elif tmp_sum > 0:
+                    rt -= 1
+                else:
+                    set_answer.add((num, nums[lt], nums[rt]))
 
-        return answer
+                    lt += 1
+                    rt -= 1
+
+        return list(set_answer)
+
+
+print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
