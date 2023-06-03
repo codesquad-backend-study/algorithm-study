d76aedc2c9d266a83a6b20fcaf4da178c7f00ef0
Hyun <ghkdgus29@naver.com>
2023-06-03 20:06:54 +0900

    Hyun: 316 풀이

--- programmers/Hyun/week17/316.py
+++ programmers/Hyun/week17/316.py
@@ -1,16 +1,17 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-
-        dic = {}
-        stack = []
+        counter, seen, stack = collections.Counter(s), set(), []
 
         for ch in s:
-            if ch in dic:
-                for idx in range(len(stack)):
-                    if ord(stack[idx]) < ord(ch):
-                        stack.remove(ch)
-                        break
-            dic[ch] = 0
+            counter[ch] -= 1
+
+            if ch in seen:
+                continue
+
+            while stack and ch < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+
             stack.append(ch)
+            seen.add(ch)
 
         return ''.join(stack)
