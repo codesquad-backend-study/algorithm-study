3b955757fd8400195267d364f3a73672b54daeef
Sully <2dcoder@naver.com>
2023-06-17 15:07:07 +0900

    Sully: LeetCode / 46번 - 미디움 / Python / S2. dfs 사용 (실패)

--- leetcode/sully/week19/L46.py
+++ leetcode/sully/week19/L46.py
@@ -1,7 +1,20 @@
-import itertools
 from typing import List
 
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return itertools.permutations(nums)
+        answer = []
+
+        def dfs(elements: List[int]) -> None:
+            if len(elements) == len(nums):
+                answer.append(elements)
+
+            for num in nums:
+                if num not in elements:
+                    elements.append(num)
+                    dfs(elements)
+
+        for i in range(len(nums)):
+            dfs([nums[i]])
+
+        return answer
