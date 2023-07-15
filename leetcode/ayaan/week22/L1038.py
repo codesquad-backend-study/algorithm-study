faedb767b2c7d5e26638e72933c829e60fff8c75
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-07-15 16:27:16 +0900

    Ayaan : LeetCode 1038

---
+++ leetcode/ayaan/week22/L1038.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    sum = 0
+
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        def dfs(root):
+            if root.right:
+                dfs(root.right)
+
+            self.sum += root.val
+            root.val = self.sum
+
+            if root.left:
+                dfs(root.left)
+
+        dfs(root.right)
+        self.sum += root.val
+        root.val = self.sum
+        if root.left:
+            dfs(root.left)
+
+        return root
