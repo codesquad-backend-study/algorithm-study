2ec49357f4a133d2ecd7d2ca75f7dbbea5f370d4
Sully <2dcoder@naver.com>
2023-07-22 14:34:32 +0900

    Sully: LeetCode / 105번 - 미디움 / Python / 성공

--- leetcode/sully/week23/L105.py
+++ leetcode/sully/week23/L105.py
@@ -13,22 +13,23 @@ class TreeNode:
 class Solution:
     # 주어진 inorder, preorder 값으로 트리 만드는 문제
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder:
+        if not inorder:
             return None
 
         # preorder ALWAYS has the node first.
         # But you don't know the size of either branch.
-        pre_popleft = collections.deque(preorder).popleft()
+        pre_popleft = preorder.pop(0)
+        # pre_popleft = collections.deque(preorder).popleft()
 
         # inorder ALWAYS has the left branch to the left of the node
         # and right branch to the right of the node.
         # So now you know the size of each branch.
-        index = inorder.index(pre_popleft)
-        # for x in inorder:
-        #     index += 1
-        #
-        #     if x == pre_popleft:
-        #         break
+        index = 0
+        for x in inorder:
+            if x == pre_popleft:
+                break
+
+            index += 1
 
         node = TreeNode(inorder[index])
 
