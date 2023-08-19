67c364db4882c91eff8051e6579b7578d289ac8a
Hyun <ghkdgus29@naver.com>
2023-08-19 13:24:08 +0900

    Hyun: 136

---
+++ programmers/Hyun/week27/136.py
@@ -0,0 +1,11 @@
+import collections
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+
+        for num in nums:
+            ans ^= num
+
+        return ans
