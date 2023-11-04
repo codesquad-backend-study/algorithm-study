1e304756da1ef00dfd689fd5bd3b1e4e25b4dfa0
Hyun <ghkdgus29@naver.com>
2023-11-04 17:03:18 +0900

    Hyun: 합승 택시 요금 개선

---
+++ programmers/Hyun/week35/합승 택시 요금 (우선순위 큐).py
@@ -0,0 +1,62 @@
+import heapq
+
+
+def solution(n, s, a, b, fares):
+    NOT_VISIT = 10_0000 * 200 * 10
+
+    graph = [[] for _ in range(n + 1)]
+
+    for n1, n2, fare in fares:
+        graph[n1].append((n2, fare))
+        graph[n2].append((n1, fare))
+
+    def bfs(start, end):
+        dist = [NOT_VISIT] * (n + 1)
+
+        queue = []
+        heapq.heappush(queue, (0, start))
+        dist[start] = 0
+
+        while queue:
+            weight, cur = heapq.heappop(queue)
+
+            if dist[cur] < weight:
+                continue
+
+            for node, fare in graph[cur]:
+                if dist[node] > dist[cur] + fare:
+                    dist[node] = dist[cur] + fare
+                    heapq.heappush(queue, (dist[cur] + fare, node))
+
+        return dist[end]
+
+    def calculate_joint_cost():
+        dist = [NOT_VISIT] * (n + 1)
+
+        queue = []
+        heapq.heappush(queue, (0, s))
+        dist[s] = 0
+
+        while queue:
+            weight, cur = heapq.heappop(queue)
+
+            if dist[cur] < weight:
+                continue
+
+            for node, fare in graph[cur]:
+                if dist[node] > dist[cur] + fare:
+                    dist[node] = dist[cur] + fare
+                    heapq.heappush(queue, (dist[cur] + fare, node))
+
+        return dist
+
+    def caculate_total_cost(joint_node):
+        return bfs(joint_node, a) + bfs(joint_node, b) + joint_cost[joint_node]
+
+    joint_cost = calculate_joint_cost()
+
+    ans = NOT_VISIT
+    for joint_node in range(1, n + 1):
+        ans = min(ans, caculate_total_cost(joint_node))
+
+    return ans
