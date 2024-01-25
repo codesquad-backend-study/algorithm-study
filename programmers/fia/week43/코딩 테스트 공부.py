2aee5c1de518422834d7a7b5d1a7c1bc9d783e6b
yeonise <cerealnum@gmail.com>
2024-01-25 14:33:46 +0900

    Fia : 코딩 테스트 공부 / Programmers

---
+++ programmers/fia/week43/코딩 테스트 공부.py
@@ -0,0 +1,28 @@
+def solution(alp, cop, problems):
+    max_alp = 0
+    max_cop = 0
+
+    for alp_req, cop_req, _, _, _ in problems:
+        max_alp = max(max_alp, alp_req)
+        max_cop = max(max_cop, cop_req)
+
+    times = [[float('inf')] * (max_cop + 1) for _ in range(max_alp + 1)]
+    alp = min(max_alp, alp)
+    cop = min(max_cop, cop)
+    times[alp][cop] = 0
+
+    for a in range(alp, max_alp + 1):
+        for c in range(cop, max_cop + 1):
+            if a + 1 <= max_alp:
+                times[a + 1][c] = min(times[a][c] + 1, times[a + 1][c])
+            if c + 1 <= max_cop:
+                times[a][c + 1] = min(times[a][c] + 1, times[a][c + 1])
+
+            for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+                if a >= alp_req and c >= cop_req:
+                    na = min(max_alp, a + alp_rwd)
+                    nc = min(max_cop, c + cop_rwd)
+
+                    times[na][nc] = min(times[a][c] + cost, times[na][nc])
+
+    return times[-1][-1]
