43d619a1f3b6f4e3cb65bfc82569628829fc2d57
yeonise <cerealnum@gmail.com>
2024-02-15 17:13:15 +0900

    Fia : 2020 카카오 인턴십 / 동굴 탐험 해결

--- programmers/fia/week46/동굴 탐험.py
+++ programmers/fia/week46/동굴 탐험.py
@@ -1,13 +1,48 @@
 from collections import deque
 
+# def solution(n, path, order):  # 미해결
+#     route = [[] for _ in range(n)]
+#     visited = [False] * n
+#
+#     for a, b in path:
+#         route[a].append(b)
+#         route[b].append(a)
+#
+#     key = [0] * n
+#     lock = [0] * n
+#
+#     for a, b in order:
+#         key[a] = b
+#         lock[b] = a
+#
+#     queue = deque([0])
+#     revisits = set()
+#
+#     while queue:
+#         room = queue.popleft()
+#
+#         visited[room] = True
+#
+#         if key[room] != 0:  # 자신이 key 방이라면 lock 방 열어주기
+#             lock[key[room]] = 0
+#
+#         for other_room in route[room]:
+#             if not visited[other_room] and lock[other_room] == 0:
+#                 queue.append(other_room)
+#             else:  # 잠겨있는 방이라면 재방문 표시 해두기
+#                 revisits.add(other_room)
+#
+#     return sum(visited) == n
 
-def solution(n, path, order):  # 미해결
-    route = [[] for _ in range(n)]
-    visited = [False] * n
+from collections import deque
+
+
+def solution(n, path, order):  # 해결
+    graph = [[] for _ in range(n)]
 
     for a, b in path:
-        route[a].append(b)
-        route[b].append(a)
+        graph[a].append(b)
+        graph[b].append(a)
 
     key = [0] * n
     lock = [0] * n
@@ -16,22 +51,32 @@ def solution(n, path, order):  # 미해결
         key[a] = b
         lock[b] = a
 
+    visited = [False] * n
+
+    ##########
+
     queue = deque([0])
     revisits = set()
 
     while queue:
-        room = queue.popleft()
+        current = queue.popleft()
 
-        visited[room] = True
+        if lock[current] != 0:  # 잠겨있는 방을 방문했다면 다시 방문할 목록에 기록하기
+            revisits.add(current)
+            continue
 
-        if key[room] != 0:  # 자신이 key 방이라면 lock 방 열어주기
-            lock[key[room]] = 0
+        visited[current] = True
 
-        for other_room in route[room]:
-            if not visited[other_room] and lock[other_room] == 0:
-                queue.append(other_room)
-            else:  # 잠겨있는 방이라면 재방문 표시 해두기
-                revisits.add(other_room)
+        if key[current] != 0:  # key가 되는 방을 방문했으므로 이어진 다음 방 열기
+            lock[key[current]] = 0
+
+            if key[current] in revisits:  # 잠겨있는 방을 먼저 방문했는지 확인하기
+                queue.append(key[current])
+                revisits.remove(key[current])
+
+        for child in graph[current]:
+            if not visited[child]:
+                queue.append(child)
 
     return sum(visited) == n
 
